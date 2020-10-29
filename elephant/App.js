f04509b8ab12af39c29648679d306dbf5d78a503
sahilsian <sahilsian101@gmail.com>
2020-10-28 18:16:20 -0700

    Initial Master Setup/Commit

---
+++ elephant/App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import MyTestComp from './comps/Test';
+import {View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <MyTestComp />
+    </View>
+  )
+}
+
+export default App;
+
+//export {default} from './storybook';
---
+++ elephant/comps/Test/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const MyTestComp = () => {
+    return <View>
+        <Text>Hello World!</Text>
+    </View>
+}
+
+export default MyTestComp;
