ee211926432c9c70e5438ce4d1e7dab671f8f7d4
sahilsian <sahilsian101@gmail.com>
2020-11-02 12:05:03 -0800

    moving computer commit

---
+++ elephant/comps/CustomTextInput/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+const CustomInput = ({}) => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default CustomInput;
