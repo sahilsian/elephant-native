e36edc7461cac45df23def7f8cb931375a72c84b
KaiErfle <kaierfle@gmail.com>
2020-11-04 18:18:05 -0800

    NavBar

---
+++ elephant/comps/NavBar/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled, { css } from "styled-components/native";
+import { View, Image, AppRegistry } from "react-native";
+
+//in codesandbox I added the images to the same folder but still got an error
+
+const MainDiv = styled.View`
+  width: 400px;
+  background-color: white;
+  box-shadow: 2px 2px 5px gray;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 15px 0px 15px;
+`;
+
+export const NavBar = ({}) => {
+  return (
+    <View>
+      <MainDiv>
+        hi
+        <Image src={('./NavBar/Home (2).png')} />
+        {/* <Image src="/Search (1).png"></Image>
+        <Image src="/calender (1).png"></Image>
+        <Image src="/Chat (1).png"></Image> */}
+      </MainDiv>
+    </View>
+  );
+};
+
+export default NavBar;
