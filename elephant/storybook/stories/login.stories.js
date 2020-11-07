f89a556fa331df7a56a2543e16cfad79bda2e6a3
sahilsian <sahilsian101@gmail.com>
2020-11-06 23:41:54 -0800

    login story

--- elephant/storybook/stories/login.stories.js
+++ elephant/storybook/stories/login.stories.js
@@ -1,10 +1,10 @@
 import { storiesOf } from '@storybook/react-native';
 import React from 'react';
 import FullView from './FullView';
-import Start from '../../comps/Pages/Start';
+import Login from '../../comps/Pages/Login';
 
 storiesOf('Pages', module)
   .addDecorator((getStory) => <FullView>{getStory()}</FullView>)
   .add('My Custom Login', () => (
-    <Start></Start>
+    <Login></Login>
   ))
